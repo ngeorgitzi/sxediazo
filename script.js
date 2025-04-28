afd70a68c56b70210dc2c7c76a64134fef0539b8
Ναταλία Θεόκλεια Γεωργιτζίκη <38490566+ngeorgitzi@users.noreply.github.com>
2025-04-28 16:24:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const canvas = document.getElementById('drawing-canvas');
+const context = canvas.getContext('2d');
+
+const backgroundImage = new Image();
+backgroundImage.src = 'page.png';
+
+backgroundImage.onload = function() {
+    canvas.width = backgroundImage.width;
+    canvas.height = backgroundImage.height;
+
+    context.drawImage(backgroundImage, 0, 0);
+};
+
+let painting = false;
+let erasing = false;
+
+function startPosition(e) {
+    if (erasing) {
+        context.globalCompositeOperation = 'destination-out'; // Χρησιμοποιεί τη λειτουργία destination-out για σβήσιμο
+    } else {
+        context.globalCompositeOperation = 'source-over'; // Χρησιμοποιεί τη λειτουργία source-over για σχεδίαση
+    }
+
+    painting = true;
+    draw(e);
+}
+
+function endPosition() {
+    painting = false;
+    context.beginPath();
+}
+
+function draw(e) {
+    if (!painting) return;
+
+    context.lineWidth = document.getElementById('brush-size').value;
+    context.lineCap = 'round';
+    context.strokeStyle = erasing ? '#FFFFFF' : document.getElementById('brush-color').value;
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    context.lineTo(x, y);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(x, y);
+}
+
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', endPosition);
+canvas.addEventListener('mousemove', draw);
+
+// Προσθήκη λειτουργικότητας για το κουμπί καθαρισμού
+const clearButton = document.getElementById('clear-button');
+
+clearButton.addEventListener('click', function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(backgroundImage, 0, 0);
+});
+
+// Προσθήκη λειτουργικότητας για το toggle κουμπί σβήσιμου
+const eraseToggle = document.getElementById('erase-toggle');
+
+eraseToggle.addEventListener('change', function() {
+    erasing = eraseToggle.checked;
+});
